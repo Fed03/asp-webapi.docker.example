b662c85a3ced2ff00c5cd5ecfd86fc5d5832c62d
Federico Teotini <federico.teotini@remira.com>
2025-03-24 14:15:32 +0100

    Env variable

--- services/WeatherService.cs
+++ services/WeatherService.cs
@@ -1,22 +1,40 @@
-﻿namespace services;
+﻿using Microsoft.Extensions.Configuration;
+
+namespace services;
 
 
 public class WeatherService
 {
-    private static readonly string[] Summaries =
+    private readonly string? _lang;
+
+    public WeatherService(IConfiguration config)
+    {
+        _lang = config["WeatherLanguage"];
+    }
+    
+    private static readonly string[] EnSummaries =
     [
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+    
+    
+    private static readonly string[] ItSummaries =
+    [
+        "Gelido", "Freddo", "MEdio", "Caldo", "Caldissimo", "Rovente"
     ];
     
     public IEnumerable<WeatherForecast> GetWeatherForecast(int daysNumber)
     {
         return Enumerable.Range(1, daysNumber).Select(
                 index =>
-                    new WeatherForecast(
+                {
+                    var summaries = _lang == "it" ? ItSummaries : EnSummaries;
+                    return new WeatherForecast(
                         DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                         Random.Shared.Next(-20, 55),
-                        Summaries[Random.Shared.Next(Summaries.Length)]
-                    )
+                        summaries[Random.Shared.Next(summaries.Length)]
+                    );
+                }
             )
             .ToArray();
     }
